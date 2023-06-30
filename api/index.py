3b8601c8b631da113351f82425982b391eac9e39
avisekk <avisekksarma@gmail.com>
2023-07-01 01:35:44 +0545

    test by minimal

--- api/index.py
+++ api/index.py
@@ -10,21 +10,24 @@ client = MongoClient('mongodb://mongo:yEmfwMJ7AZDQWsToN07r@containers-us-west-12
 db = client.flask_db
 todos = db.todos
 
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method == "POST":
-        print('i am here in home- post')
-        content = request.form['content']
-        todos.insert_one({'content':content})
-        return redirect(url_for('home'))
-    all_todos = todos.find()
-    return render_template('todos.html',todos=all_todos)
+# @app.route('/',methods=['GET','POST'])
+# def home():
+#     if request.method == "POST":
+#         print('i am here in home- post')
+#         content = request.form['content']
+#         todos.insert_one({'content':content})
+#         return redirect(url_for('home'))
+#     all_todos = todos.find()
+#     return render_template('todos.html',todos=all_todos)
     
-@app.route('/delete',methods=['POST'])
-def delete():
-    if request.method == "POST":
-        print('i am here')
-        todo_id = request.form['todo_id']
-        print(todo_id)
-        todos.delete_one({"_id":ObjectId(todo_id)})
-        return redirect(url_for('home'))
+# @app.route('/delete',methods=['POST'])
+# def delete():
+#     if request.method == "POST":
+#         print('i am here')
+#         todo_id = request.form['todo_id']
+#         print(todo_id)
+#         todos.delete_one({"_id":ObjectId(todo_id)})
+#         return redirect(url_for('home'))
+@app.route('/')
+def hello():
+    return "hello there man"
